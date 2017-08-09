a906381f59b19f58d0aa6c7296471b128c465f26
themitsou <themitsou@gmail.com>
2017-08-09 12:33:10 +0300

    commit

--- app/src/main/java/gr/academic/city/sdmd/projectissues/ui/activity/fragments/ClubActivityDetailsFragment.java
+++ app/src/main/java/gr/academic/city/sdmd/projectissues/ui/activity/fragments/ClubActivityDetailsFragment.java
@@ -1,9 +1,11 @@
 package gr.academic.city.sdmd.projectissues.ui.activity.fragments;
 
+import android.animation.ObjectAnimator;
 import android.content.ContentUris;
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.LoaderManager;
@@ -15,8 +17,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-
 
 
 import java.text.SimpleDateFormat;
@@ -26,11 +29,12 @@ import gr.academic.city.sdmd.projectissues.R;
 import gr.academic.city.sdmd.projectissues.db.ProjectManagementContract;
 import gr.academic.city.sdmd.projectissues.util.Constants;
 
-public class ClubActivityDetailsFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>  {
+public class ClubActivityDetailsFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     public interface OnFragmentInteractionListener {
         void onClubActivityDeleted(Long activity);
     }
+
     private static final String ARG_PARAM1 = "param1";
     private SimpleDateFormat dateFormat = new SimpleDateFormat(Constants.PROJECT_ISSUES_DATE_FORMAT);
     private static final int CLUB_ACTIVITY_LOADER = 20;
@@ -48,7 +52,7 @@ public class ClubActivityDetailsFragment extends Fragment implements LoaderManag
 
     private OnFragmentInteractionListener mListener;
 
-     public ClubActivityDetailsFragment() {
+    public ClubActivityDetailsFragment() {
         // Required empty public constructor
     }
 
@@ -60,8 +64,6 @@ public class ClubActivityDetailsFragment extends Fragment implements LoaderManag
         fragment.setArguments(args);
         return fragment;
     }
-
-
 
 
     @Override
@@ -79,18 +81,38 @@ public class ClubActivityDetailsFragment extends Fragment implements LoaderManag
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view =  inflater.inflate(R.layout.fragment_club_activity_details, container, false);
+        view = inflater.inflate(R.layout.fragment_club_activity_details, container, false);
 
         tvTitle = (TextView) view.findViewById(R.id.tv_club_activity_title);
         tvShortNote = (TextView) view.findViewById(R.id.tv_club_activity_short_note);
         tvLongNote = (TextView) view.findViewById(R.id.tv_club_activity_long_note);
         tvDate = (TextView) view.findViewById(R.id.tv_club_activity_date);
 
+        final ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        final ObjectAnimator animation = ObjectAnimator.ofInt(progressBar, "progress", 1000, 0); // see this max value coming back here, we animate towards that value
+
+
+        final FloatingActionButton myFab = (FloatingActionButton) view.findViewById(R.id.floatingActionButton);
+        myFab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (animation.isRunning()) {
+                    animation.end();
+                    myFab.setImageResource(android.R.drawable.ic_media_play);
+                }else{
+                    animation.setDuration(1500000); //in milliseconds
+                    animation.setInterpolator(new DecelerateInterpolator());
+                    animation.start();
+                    myFab.setImageResource(android.R.drawable.checkbox_off_background);
+                }
+
+            }
+
+        });
+
         getLoaderManager().initLoader(CLUB_ACTIVITY_LOADER, null, this);
 
         return view;
     }
-
 
 
     @Override
@@ -137,9 +159,6 @@ public class ClubActivityDetailsFragment extends Fragment implements LoaderManag
         updateView(null);
     }
 
-   
-
-
 
     private void updateView(Cursor cursor) {
         if (cursor != null && cursor.moveToFirst()) {
@@ -147,13 +166,14 @@ public class ClubActivityDetailsFragment extends Fragment implements LoaderManag
             tvShortNote.setText(cursor.getString(cursor.getColumnIndexOrThrow(ProjectManagementContract.ProjectIssue.COLUMN_NAME_SHORT_NOTE)));
             tvLongNote.setText(cursor.getString(cursor.getColumnIndexOrThrow(ProjectManagementContract.ProjectIssue.COLUMN_NAME_LONG_NOTE)));
             tvDate.setText(dateFormat.format(new Date(cursor.getLong(cursor.getColumnIndexOrThrow(ProjectManagementContract.ProjectIssue.COLUMN_NAME_TIMESTAMP)))));
-                   }
+
+
+        }
 
         if (cursor != null) {
             cursor.close();
         }
     }
-
 
 
     @Override
